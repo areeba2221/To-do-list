452fb7c53ba481c0476b594bdc2b2150f4f7b6c6
areebafatima2205 <areebafatima2205@gmail.com>
2026-04-28 20:42:46 +0500

    new design

---
+++ script.js
@@ -0,0 +1,9 @@
+let inputs = document.getElementById("input");
+let text = document.getElementById("text");
+let btn = document.getElementById("inp")
+
+function add() {
+    if(btn.value == "") {
+        alert = ("Enter the task")
+    }
+}
